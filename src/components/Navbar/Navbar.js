c019eefeaa2cfd67d5a3e3da15ad10a1882d44f7
udaypatel1 <uday3patel@gmail.com>
2023-08-26 22:26:45 -0400

    fixed "contact" font

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
         <ul className="nav-links">
           <li><a href="#" className="fontify">About</a></li>
           <li><a href="#" className="fontify">Resume</a></li>
-          <li><a href="#" className="fontifys">Contact</a></li>
+          <li><a href="#" className="fontify">Contact</a></li>
         </ul>
       </nav>
       
