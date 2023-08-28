f5366f6bcd248382ebc141d775449b664f5c8671
udaypatel1 <uday3patel@gmail.com>
2023-08-27 23:05:03 -0400

    optimized landing page for iphone. research on creating a global .css for tag styles because it applies to all tags across project

--- src/About.js
+++ src/About.js
@@ -6,18 +6,22 @@ import Navbar from './components/Navbar/Navbar';
 function AboutPage() {
     return (
 
-        <div className="About-body">
-
-            <div className="About-header">
-
-                <Navbar class="about-nav" />
-                <h1 className="bottom-line">Hi! My name is <span class="orangify">Uday</span></h1>
-
-                <h2><span class="orangify">. . .</span> this page is still in <span class="orangify">development</span>. Thanks for your patience!</h2>
-
-            </div>
+        <div className="page">
+            <header className="header">
+                <Navbar />
+            </header>
+                <main className="main-content">
+                    <section className="section">
+                        <h3>Section 1</h3>
+                        <p>Content here...</p>
+                    </section>
+                    <section className="section">
+                        <h2>Section 2</h2>
+                        <p>More content...</p>
+                    </section>
+                </main>
+            <footer className="footer">Footer</footer>
         </div>
-
         
     );
   }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,6 +8,7 @@ function Navbar() {
       <nav className="navbar">
         
         <ul className="nav-links">
+          <li><Link to="/" className="poppins-regular">Home</Link></li>
           <li><Link to="/about" className="poppins-regular">About</Link></li>
           <li><Link to="/resume" className="poppins-regular">Resume</Link></li>
           <li><Link to="/contact" className="poppins-regular">Contact</Link></li>
