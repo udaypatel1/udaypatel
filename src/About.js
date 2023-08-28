328cbd76d110a62bdf7d36da854e94514857d4e3
udaypatel1 <uday3patel@gmail.com>
2023-08-27 21:34:32 -0400

    h2 edits for about page

--- src/About.js
+++ src/About.js
@@ -11,8 +11,11 @@ function AboutPage() {
             <div className="About-header">
 
                 <Navbar class="about-nav" />
-                <h1>Hi! My name is <span class="orangify">Uday</span></h1>
+                <h1 className="bottom-line">Hi! My name is <span class="orangify">Uday</span></h1>
 
+                <h2>I graduated from <span class="orangify">Rutgers University</span> in <span class="orangify">2022</span></h2>
+                <h2>I'm currently a <span class="orangify">Software Engineer</span> at <span class="orangify">Guardian Life</span> in <span class="orangify">NYC</span></h2>
+                <h2>Feel free to view some of my <span class="orangify">interests</span></h2>
                 
             </div>
         </div>
